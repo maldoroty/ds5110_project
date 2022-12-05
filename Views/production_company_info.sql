7999fde25f341a1474531bd75d08ac657376b8fd
hajsid <hajerahs@gmail.com>
2022-12-05 14:52:52 -0500

    editted production company view

--- Views/production_company_info.sql
+++ Views/production_company_info.sql
@@ -3,6 +3,8 @@ USE streaming_service_db;
 DROP VIEW IF EXISTS ‘pc_public_info’;
 
 create view pc_public_info as
-	select contract_id, p_id
-    from contract;
+	select c.contract_id, c.p_id, p.name as production_company
+    from contract c, production_company p
+    where c.p_id = p.p_id
+    group by c.contract_id, c.p_id, p.name;
     
